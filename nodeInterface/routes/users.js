d45a6dd36c09099266cff59890f59f623a9e9ce4
tanliangbang <648103576@qq.com>
2017-03-09 18:08:21 +0800

    2017-3-9

--- nodeInterface/routes/users.js
+++ nodeInterface/routes/users.js
@@ -22,6 +22,36 @@ router.post('/login', function(req, res, next) {
 });
 
 
+router.post('/register', function(req, res, next) {
+    var sql = "select id from bang_users where username= '"+req.body.username +"'";
+    db.query(sql, function(err, rows, fields){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        if(rows.length>0){
+            utilFn.successSend(res,null,500,'该用户已存在');
+        }else{
+            innerUser(req, res, next);
+        }
+    });
+});
+
+
+function innerUser(req, res, next){
+    console.log("aaaaaaaaaaaaaa")
+    var sql = "insert into bang_users (username,password,email) values "+
+        "('"+req.body.username+"','"+req.body.password+"','"+ req.body.email+"')";
+    db.query(sql, function(err, rows, fields){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        utilFn.successSend(res,null,200,'注册成功');
+
+    });
+}
+
 router.get('/getUserInfo', function(req, res, next) {
    utilFn.successSend(res,req.session.user,200);
 });
